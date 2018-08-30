d97db73d1ae1edef01bae77b1c43d9fd02eff050
Ashutosh Singh <31001615+ashu1001@users.noreply.github.com>
2018-08-30 09:13:06 +0530

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -10,15 +10,14 @@ if (!file.exists("exdata_data_household_power_consumption")) {
 
 
 #### Reading Plot Data and subsetting based on the date
-plotData <- read.table("./household_power_consumption.txt",header = TRUE,sep = ";")
+plotData <- read.table("./household_power_consumption.txt",header = TRUE,sep = ";",na.string="?")
 plotData$Date <- as.Date(plotData$Date,format = "%d/%m/%Y")
 plotData.new <- subset(plotData,Date == "2007-02-01" | Date == "2007-02-02")
 
 #### Converting Active power to kilowatts
 plotData.new$Global_active_power = as.numeric(plotData.new$Global_active_power)
-powerKW <- sapply(plotData.new$Global_active_power,function(x){x/1000})
 
 #### Constructing the plot and converting into png file
-hist(powerKW,col = "red",xlab = "Global Active Power(kilowatts)",main = "Global Active Power")
+hist(plotData.new$Global_active_power,col = "red",xlab = "Global Active Power(kilowatts)",main = "Global Active Power")
 dev.copy(png, file="plot1.png", width=480, height=480)
 dev.off()
